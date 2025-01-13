99e7acddb750cb6f66034a65c587d68d27a60620
ccodderr <crptcharm@gmail.com>
2025-01-13 21:41:36 +0100

    Реализована авторизация через WebView

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,50 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Yana Silosieva on 13.01.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    private let authScreenLogo = UIImage(named: "logo_of_Unsplash")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else {
+                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+                return
+            }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(systemName: "chevron.backward")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(systemName: "hevron.backward")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
+}
